a66cea5b80bcce9e783362d0c4720bac79033a74
Abdalla Osama <abdallahosama393@gmail.com>
2026-09-08 01:35:16 +0800

    Fix run.py and ensure requirements for Railway

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 """Convenience entrypoint: `python run.py`"""
+import os
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8001, reload=True)
+    port = int(os.environ.get("PORT", 8080))
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port)
